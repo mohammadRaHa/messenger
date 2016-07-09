9205dff8f13c3c1ac9fafeb4820791400227fd77
RaHa <mohammadraha313@gmail.com>
2016-07-09 05:55:58 +0430

    Create var.php

---
+++ func/var.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: raha
+ * Date: 08/07/16
+ * Time: 22:32
+ */
+define("HOST", "localhost");
+define("DB", "messenger");
+define("USER", "admin34jp4Jj");
+define("PASSWORD", "mnALDW_I1Cd4");
+?>
